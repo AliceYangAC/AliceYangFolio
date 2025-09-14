08b6e51f12ceb9b8deb0260f31154b65e67d44ca
AliceYangAC <yang0447@algonquinlive.com>
2025-09-14 17:03:30 -0400

    fixed port binding for render deployment

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, json
+import os
 
+port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
 
 @app.route("/")
@@ -25,4 +27,4 @@ def index():
     return render_template("index.html", repos=repos, all_tags=all_tags, selected_tags=selected_tags)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
