bdffd93f9a9033c0d67c8ee269ab2d62c58fb78f
AliceYangAC <yang0447@algonquinlive.com>
2025-09-15 21:44:48 -0400

    added README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# AliceProjects
+# WhisperSRTube
 
+My Python-based portfolio web page built on Flask with custom styling.  
+
+---
+
+## Features
+
+- Dynamic project filtering with Javascript
+- Desktop & mobile friendly UI
+  
+---
+
+## Future Plans
+
+1. Contact Form
+2. Introduction Page
+
+---
+
+## License
+
+This project is released under the MIT License. See [LICENSE](LICENSE) for details.
--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request, json
 import os
 
-port = int(os.environ.get("PORT", 10000))  # fallback to 10000 if PORT isn't set
+# Get port from environment variable or default to 10000 (for Render)
+port = int(os.environ.get("PORT", 10000))  
 
 app = Flask(__name__)
 
+# Define route to handle requests to the root URL ('/')
 @app.route("/")
 def index():
+    # Get selected tags from query parameters when user selects tags in the filter
     tag_query = request.args.get("tags", "")
     selected_tags = tag_query.split(",") if tag_query else []
 
+    # Load repository data from repos.JSON file
     with open("repos.json") as f:
         all_repos = json.load(f)
 
@@ -22,7 +26,7 @@ def index():
     else:
         repos = all_repos
 
-    # Extract all unique tags for the filter bar
+    # Extract all unique tags for the filter bar, ordered alphabetically
     all_tags = sorted({tag for repo in all_repos for tag in repo.get("tags", [])})
 
     return render_template("index.html", repos=repos, all_tags=all_tags, selected_tags=selected_tags)
